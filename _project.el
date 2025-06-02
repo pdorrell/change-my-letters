60de455858e93af882c7a5fd45de7476dd3bb13d
Philip <philip@thinkinghard.com>
2025-06-02 12:58:48 +1200

    tweak project setup to work with emacs setup

--- _project.el
+++ _project.el
@@ -3,5 +3,4 @@
 (load-this-project
  `( (:project-type typescript)
     (:project-name "change-my-letters")
-    (:alternate-file-or-dir-command (other-window base-dir "run-npm-test" this-file-or-dir ("NPM_TEST" . file)))
     ) )
