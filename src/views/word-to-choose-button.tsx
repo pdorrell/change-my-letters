e3b38207cc6458e192ddc9c5a7be352e9c4af3c0
Philip <philip@thinkinghard.com>
2025-06-05 21:11:49 +1200

    styling tweaks

--- src/views/word-to-choose-button.tsx
+++ src/views/word-to-choose-button.tsx
@@ -7,7 +7,7 @@ interface WordToChooseButtonProps { wordToChoose: WordToChoose; }
 export const WordToChooseButton: React.FC<WordToChooseButtonProps> = observer(({ wordToChoose }) => {
   return (
     <span
-      className={`word-span ${wordToChoose.enabled ? '' : 'disabled'}`}
+      className={`word-to-choose-button ${wordToChoose.enabled ? '' : 'disabled'}`}
       onClick={wordToChoose.enabled ? () => wordToChoose.choose() : undefined}
       title={wordToChoose.enabled ? `Choose "${wordToChoose.word}"` : 'Choose a word to find first'}
     >
